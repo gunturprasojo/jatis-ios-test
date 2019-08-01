5051a3ef9c58f60bc88731eea03d700c05c40f1e
Guntur Budi <guntur.budi19@gmail.com>
2019-08-02 00:09:13 +0700

    update 3

---
+++ Pods/Jatis-iOS/JatisPopUp/JatisPopUpTable.swift
@@ -0,0 +1,205 @@
+//
+//  JatisPopUpTable.swift
+//  Banner
+//
+//  Created by Guntur Budi on 25/06/19.
+//  Copyright © 2019 Jati Piranti Solusindo. All rights reserved.
+//
+
+import UIKit
+
+open class JatisPopUpTable: UIView {
+    
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+    //  Create an array type of String that want to show.
+    open var strings: [String] = []
+    
+    // Title of Pop Up
+    open var title: String = ""
+    
+    //  Create Size of parent of table view
+    open var parentSize: CGSize = CGSize(width: 0, height: 0)
+    
+    //  Create an array of String -> Id of an image.
+    open var tableSize: CGSize = CGSize(width: 0, height: 0)
+    
+    //  Create a float type variable to set corner radius,
+    //  that used to set the radius of the corner of a view.
+    open var cornerRadius: CGFloat = 10.0
+    
+    
+    // Create variable color to set title background
+    open var titleBackroundColor : UIColor = .white
+    
+    // Create variable color to set backround of content color
+    open var contentBackgroundColor : UIColor = .white
+    
+    // Create variable color to set title color
+    open var titleTextColor : UIColor = .darkGray
+    
+    // Create variable color to set content text color
+    open var contentTextColor : UIColor = .darkGray
+    
+    // Create variable color to set close button color
+    open var closeButtonColor : UIColor = .darkGray
+    
+    // Create variable to set is selectable cell
+    open var isSelectable : Bool = true
+    
+    // Create variable to set is close button available in title
+    open var isUseCloseButton : Bool = true
+    
+    // Create variable height to set is height of content
+    open var cellHeight : CGFloat = 50
+    
+    // Create variable height to set is height of title
+    open var titleHeight : CGFloat = 50
+    
+    // Create variable string to set title close
+    open var closeButtonTitle : String = "x"
+    
+    
+    //===== update .2
+    
+    // Create variable to set is scrollable table
+    open var isScrollable : Bool = false
+    
+    // Create variable to set color separator betweeen title and cell
+    open var separatorTitleColor : UIColor = .gray
+    
+    // Create variable to set height of separator betweeen title and cell
+    open var separatorTitleHeight : CGFloat = 0.5
+    
+    // Create variable to set height of separator betweeen title and cell
+    open var maximumDisplayedCell : CGFloat = 5
+    
+    
+    // Declare a delegate to receiver an action of tableview
+    public weak var delegate: JatisPopUpTableProtocol?
+    
+    
+    open var tableView = UITableView()
+    
+    open var closeButton = UIButton()
+    
+    open var selectionStyle : UITableViewCell.SelectionStyle = .blue
+    
+    open func formatPopUp(){
+        self.frame =  CGRect(x: 0, y: 0, width: parentSize.width, height: parentSize.height)
+        self.layer.masksToBounds = true
+        let gray = UIColor.darkGray.withAlphaComponent(0.5)
+        self.backgroundColor = gray
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = self.cornerRadius
+        self.formatTableView()
+    }
+    
+    private func generateActions(){
+        let tap = UITapGestureRecognizer(target: self, action: #selector(singleTap(sender:)))
+        self.addGestureRecognizer(tap)
+    }
+    
+    @objc private func singleTap(sender: UITapGestureRecognizer? = nil) {
+        if(sender?.numberOfTouches == 1){
+            self.removeFromSuperview()
+        }
+    }
+    
+}
+
+
+extension JatisPopUpTable : UITableViewDataSource, UITableViewDelegate{
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.strings.count
+    }
+    
+    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let viewSection = UIView(frame: CGRect(x: 0, y: 0, width:self.tableView.frame.size.width, height: self.titleHeight))
+        let titleLbl = UILabel(frame: CGRect(x: 0, y: 0, width:self.tableView.frame.size.width, height: self.titleHeight))
+        viewSection.backgroundColor = titleBackroundColor
+        titleLbl.textAlignment = .center
+        titleLbl.text = title
+        titleLbl.textColor = titleTextColor
+        viewSection.addSubview(titleLbl)
+        if isUseCloseButton {
+            closeButton.frame = CGRect(x: self.tableView.frame.size.width - 50, y: 0, width: 50, height: self.titleHeight)
+            closeButton.addTarget(self, action: #selector(self.close), for: .touchDown)
+            closeButton.setTitle(closeButtonTitle, for: .normal)
+            closeButton.setTitleColor(self.closeButtonColor, for: .normal)
+            viewSection.addSubview(closeButton)
+        }
+        let separatorSection = UIView(frame: CGRect(x: 0, y: self.titleHeight, width:self.tableView.frame.size.width, height: separatorTitleHeight))
+        separatorSection.backgroundColor = separatorTitleColor
+        viewSection.addSubview(separatorSection)
+        return viewSection
+    }
+    
+    @objc func close(){
+        self.removeFromSuperview()
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return self.titleHeight
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let popUpCell = tableView.dequeueReusableCell(withIdentifier: "jatisPopUpTableCell", for: indexPath)
+        let titleLbl = UILabel(frame: CGRect(x: 0, y: 0, width:self.tableView.frame.size.width, height: self.cellHeight))
+        titleLbl.backgroundColor = contentBackgroundColor
+        titleLbl.textAlignment = .center
+        titleLbl.text = strings[indexPath.row]
+        titleLbl.textColor = contentTextColor
+        popUpCell.addSubview(titleLbl)
+        popUpCell.selectionStyle = self.selectionStyle
+        return popUpCell
+    }
+    
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.didSelect(strings[indexPath.row], row: indexPath.row)
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return self.cellHeight
+    }
+    
+    private func formatTableView(){
+        let heightTable = self.maximumDisplayedCell * self.cellHeight + self.titleHeight
+        self.tableView = UITableView(frame: CGRect(x: parentSize.width/2 - self.tableSize.width/2, y:  parentSize.height/2 - self.tableSize.height/2, width: self.tableSize.width, height: heightTable), style: .plain)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "jatisPopUpTableCell")
+        tableView.isScrollEnabled = isScrollable
+        self.addSubview(tableView)
+        self.tableView.layer.cornerRadius = self.cornerRadius
+        self.tableView.clipsToBounds = true
+        
+        if isSelectable {
+            tableView.allowsSelection = true
+        }else {
+            tableView.allowsSelection = false
+        }
+        
+        tableView.reloadData()
+    }
+    
+    private func formatCell(){
+        
+    }
+    
+    open func formatCloseButton(){
+        
+    }
+}
+
+public protocol JatisPopUpTableProtocol: class {
+    func didSelect(_ data: String , row : Int)
+}
+
+
---
+++ Pods/Jatis-iOS/JatisTextView/JatisTextView.swift
@@ -0,0 +1,217 @@
+//
+//  JatisTextView.swift
+//  
+//
+//  Created by Guntur Budi on 31/07/19.
+//  Copyright © 2019 Jati Piranti Solusindo. All rights reserved.
+//
+
+import UIKit
+
+public protocol JatisTextViewProtocol: class {
+    func didJatisTextViewBeginEditing(_ data: String, tagTextView: Int)
+    func didJatisTextViewEndEditing(_ data: String, tagTextView: Int)
+    func didJatisTextViewChange(_ data: String, tagTextView: Int)
+    func didJatisTextViewShouldEnd(_ data : String, tagTextView: Int) -> Bool
+}
+
+
+
+open class JatisTextView: UIView {
+    
+    //  Create an Int type to define textfield tag.
+    open var tagTextView: Int = 0
+    
+    //  Create a String type that contain placeholder text that want to show.
+    open var textPlaceholder : String = ""
+    
+    // Create a bool type to set textview is editable or not
+    open var isEditable : Bool = true
+    
+    // Create a bool type to set textview is selectable or not
+    open var isSelectable : Bool = true
+    
+    //  Create an UIFont type to define placeholder text font.
+    open var fontPlaceholder : UIFont = UIFont(name: "Helvetica", size: 12)!
+    
+    // create size variable to define the size of the textfield
+    open var size: CGSize = CGSize(width: 0, height: 0)
+    
+    //  Create an UIFont type to define textfield text font.
+    open var fontTextView : UIFont  = UIFont(name: "Helvetica", size: 12)!
+    
+    //   Create an UIFont type to define placeholder text font.
+    open var textColor : UIColor = .darkText
+    
+    //   Create a UIColor type to define placeholder text color before animated.
+    open var placeHolderBeforeColor : UIColor = .gray
+    
+    //   Create an UIColor type to define placeholder after animated text font.
+    open var placeHolderAfterColor : UIColor = .darkText
+    
+    //   Create an UIColor type to define done button color above the keyboard.
+    open var doneButtonColor : UIColor = .blue
+    
+    //   Create a border style type to define textfield borderstyle
+    open var borderStyle : UITextField.BorderStyle = .roundedRect
+    
+    //   Create a bool type to define textfield use clear button or not.
+    open var shouldClearTextfield : Bool = true
+    
+    open var textAlignment : NSTextAlignment = .left
+    
+    open var isUnderlined : Bool = false
+    
+    open var underline = UIView()
+    
+    open var underlineColorBefore : UIColor = .gray
+    
+    open var underlineColorAfter : UIColor = .blue
+    
+    open var underlineHeight : CGFloat = 1.0
+    
+    
+    // Declare a delegate to receiver actions of textfield
+    public weak var delegate: JatisTextViewProtocol?
+    
+    open var labelPlaceholder = UILabel()
+    open var textView = UITextView()
+    open var buttonPeek = UIButton()
+    
+    
+    open func setJatisTextView(){
+        self.frame =  CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        self.layer.masksToBounds = false
+        //self.layer.cornerRadius = self.cornerRadius
+        self.formatTextView()
+    }
+    
+    open func animatePlaceholderToTop(){
+        self.expandPlaceholder()
+    }
+    
+    open func animatePlaceholderToBottom(){
+        self.minimizePlaceholder()
+    }
+    
+}
+
+
+extension JatisTextView {
+    private func formatTextView(){
+        textView.delegate = self
+        textView.frame = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
+        textView.textColor = self.textColor
+        textView.font = self.fontTextView
+        textView.tag = self.tagTextView
+        textView.isUserInteractionEnabled = true
+        textView.isEditable = self.isEditable
+        textView.isSelectable = self.isSelectable
+        textView.textAlignment = self.textAlignment
+        labelPlaceholder.text = textPlaceholder
+        labelPlaceholder.font = fontPlaceholder
+        labelPlaceholder.frame = CGRect(x: 5, y: self.bounds.origin.y/2+2, width: self.size.width-8, height: self.size.height-5)
+        labelPlaceholder.textColor = self.placeHolderBeforeColor
+        self.formateKeyboardOption()
+        self.addSubview(textView)
+        self.addSubview(labelPlaceholder)
+        
+        
+        if self.isUnderlined {
+            underline.frame = CGRect(x: 0, y: self.bounds.origin.y + self.size.height - 1, width: self.size.width, height: self.underlineHeight)
+            underline.backgroundColor = underlineColorBefore
+            self.addSubview(underline)
+        }
+        
+    }
+    
+    private func formateKeyboardOption(){
+        let keyboardToolbar = UIToolbar()
+        keyboardToolbar.barStyle = UIBarStyle.default
+        keyboardToolbar.isTranslucent = true
+        keyboardToolbar.sizeToFit()
+        let flexBarButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
+        let doneBarButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(self.endEditingText))
+        keyboardToolbar.setItems([ flexBarButton,doneBarButton], animated: true)
+        doneBarButton.tintColor = self.doneButtonColor
+        textView.inputAccessoryView = keyboardToolbar
+        
+    }
+    
+    @objc private func endEditingText(){
+        self.endEditing(true)
+    }
+}
+
+
+
+extension JatisTextView: UITextViewDelegate {
+    
+    
+    public func textViewDidBeginEditing(_ textView: UITextView) {
+        self.delegate?.didJatisTextViewBeginEditing(textView.text!, tagTextView: textView.tag)
+        self.minimizePlaceholder()
+        if isUnderlined {
+            self.underlineStateOn()
+        }
+    }
+    
+    public func textViewDidEndEditing(_ textView: UITextView) {
+        self.delegate?.didJatisTextViewEndEditing(textView.text!, tagTextView: textView.tag)
+        if(textView.text?.count == 0){
+            self.expandPlaceholder()
+        }
+        if isUnderlined {
+            self.underlineStateOf()
+        }
+    }
+    
+    @objc func textViewDidChange(_ textView: UITextView) {
+        self.delegate?.didJatisTextViewChange(textView.text!, tagTextView: textView.tag)
+    }
+    
+    public func textViewShouldEndEditing(_ textView: UITextView) -> Bool {
+        return self.delegate?.didJatisTextViewShouldEnd(textView.text! , tagTextView: textView.tag) ?? shouldClearTextfield
+    }
+}
+
+extension JatisTextView {
+    public func minimizePlaceholder(){
+        UIView.animate(withDuration: 0.15, animations: {
+            self.labelPlaceholder.frame = CGRect(x: 5, y: -(self.fontPlaceholder.pointSize), width:  self.bounds.size.width-5, height: self.fontPlaceholder.pointSize)// * 0.5)
+        })
+        self.labelPlaceholder.font = UIFont(name: fontPlaceholder.fontName, size: fontPlaceholder.pointSize * 0.9)
+        labelPlaceholder.textColor = self.placeHolderAfterColor
+        
+        
+    }
+    
+    public func expandPlaceholder(){
+        UIView.animate(withDuration: 0.15, animations: {
+            self.labelPlaceholder.frame = CGRect(x: 5, y: self.bounds.origin.y/2+2, width: self.bounds.size.width-8, height: self.bounds.size.height-5)
+        })
+        self.labelPlaceholder.font = fontPlaceholder
+        labelPlaceholder.textColor = self.placeHolderBeforeColor
+        
+    }
+    
+    public func underlineStateOn(){
+        UIView.animate(withDuration: 0.5,
+                       delay: 0.0,
+                       options: [.curveEaseInOut , .allowUserInteraction],
+                       animations: {
+                        self.underline.backgroundColor = self.underlineColorAfter
+        })
+        
+        
+    }
+    
+    public func underlineStateOf(){
+        UIView.animate(withDuration: 0.5,
+                       delay: 0.0,
+                       options: [.curveEaseInOut , .allowUserInteraction],
+                       animations: {
+                        self.underline.backgroundColor = self.underlineColorBefore
+        })
+    }
+}
